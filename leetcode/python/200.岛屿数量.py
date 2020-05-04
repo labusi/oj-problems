259d362bae30602269e3019ed666b29a2f5ce663
Jamiesona <kinard-k@qq.com>
2020-05-04 12:28:19 +0800

    岛屿的数量, 非最优解.

---
+++ leetcode/python/200.岛屿数量.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode.cn id=200 lang=python3
+#
+# [200] 岛屿数量
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        id = 1
+
+        xy2id, id2xy = {}, {}
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    top = xy2id.get((i - 1, j))
+                    left = xy2id.get((i, j - 1))
+                    if top is None and left is None:
+                        xy2id[(i, j)] = id
+                        id2xy[id] = [(i, j)]
+                        id += 1
+                    elif top is None and left is not None:
+                        xy2id[(i, j)] = left
+                        id2xy[left].append((i, j))
+                    elif top is not None and left is None:
+                        xy2id[(i, j)] = top
+                        id2xy[top].append((i, j))
+                    else:
+                        if top == left:
+                            xy2id[(i, j)] = top
+                        else:
+                            # 以top为基准
+                            xy2id[(i, j)] = top
+                            for xy in id2xy[left]:
+                                xy2id[xy] = top
+                            id2xy[top] += id2xy[left]
+                            del id2xy[left]
+                        id2xy[top].append((i, j))
+
+        return len(id2xy.keys())
+
+
+if __name__ == "__main__":
+    grid = [["1", "1", "1"], ["0", "1", "0"], ["1", "1", "1"]]
+    s = Solution()
+    ans = s.numIslands(grid)
+    print(ans)
+# @lc code=end
