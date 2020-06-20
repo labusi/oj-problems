68c32f37f141e024b105145d997475ea557bba0a
labusi <kinard-k@qq.com>
2020-06-20 20:25:25 +0800

    连续0区间吞并法.

--- leetcode/python/283.移动零.py
+++ leetcode/python/283.移动零.py
@@ -13,7 +13,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        self.helper2(nums)
+        self.helper3(nums)
 
     def helper1(self, nums):
         notZeroNums = []
@@ -41,9 +41,34 @@ class Solution:
                     k += 1
             j -= 1
 
+    def helper3(self, nums):
+        """
+        提高算法效率的本质, 就是避免一切不必要的计算.
+        i, j区间是一个连续0的区间, 这个区间向右移动时, 如果碰到非0的元素就和区间开始的元素交换; 碰到0元素就吞并.
+        """
+        N = len(nums)
+        i = 0
+        for j in range(N):
+            if nums[j] != 0:
+                if i != j:
+                    nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+    def helper4(self, nums):
+        """
+        这样会把0都移动到左边.
+        """
+        N = len(nums)
+        i = N - 1
+        for j in range(N-1, -1, -1):
+            if nums[j] != 0:
+                if i != j:
+                    nums[j], nums[i] = nums[i], nums[j]
+                i -= 1
+
 
 if __name__ == "__main__":
-    nums = [0, 1, 0]
+    nums = [0, 1, 0, 12, 34]
     s = Solution()
     s.moveZeroes(nums)
     print(nums)
